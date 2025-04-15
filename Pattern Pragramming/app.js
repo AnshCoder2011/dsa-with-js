a158e1d24f0608283fad5866f7ad909c2f074ac1
AnshCoder2011 <dubeysweety09@gmail.com>
2025-04-15 17:56:11 +0530

    gop gop

---
+++ Pattern Pragramming/app.js
@@ -0,0 +1,94 @@
+let prompt = require("prompt-sync")();
+
+let n = Number(prompt("Enter the number of elements: "));
+for (let i = 1; i <= n; i++) {
+  process.stdout.write("* ");
+}
+
+// Now I want pattern like this 👇🏻 take n and make n rows and columns
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= n; j++) {
+    process.stdout.write("* ");
+  }
+  console.log(); // adds a line gap
+}
+
+// Right angle triangle pattern
+// *
+// * * 
+// * * * 
+// * * * * 
+// * * * * *
+
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= i; j++) {
+    process.stdout.write("* ");
+  }
+  console.log(); // adds a line gap
+}
+
+// Right angle triangle number pattern
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= i; j++) {
+    process.stdout.write(j + " ");
+  }
+  console.log(); // adds a line gap
+}
+
+// Right angle triangle alphabets pattern
+// A
+// A B
+// A B C
+// A B C D
+// A B C D E
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= i; j++) {
+    process.stdout.write(String.fromCharCode(64 + j) + " ");
+  }
+  console.log(); // adds a line gap
+}
+
+// Inverted right angle triangle pattern
+// * * * * * *
+// * * * * *
+// * * * *
+// * * *
+// * *
+// *
+
+for (let i = n; i >= 1; i--) {
+  for (let j = 1; j <= i; j++) {
+    process.stdout.write("* ");
+  }
+  console.log(); // adds a line gap
+}
+
+// Inverted right angle mirror triangle pattern
+// * * * * * *
+//   * * * * *
+//     * * * *
+//       * * *
+//         * *
+//           *
+
+for (let i = n; i >= 1; i--) {
+  for (let j = n; j >= i; j--) {
+    process.stdout.write(" ");
+  }
+  for (let j = 1; j <= i; j++) {
+    process.stdout.write("* ");
+  }
+  console.log(); // adds a line gap
+}
